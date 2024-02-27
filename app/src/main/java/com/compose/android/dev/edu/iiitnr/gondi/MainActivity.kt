4807124aa7daf295aeca5443da2f0c7ad3575445
Devajit <patar1447@gmail.com>
2024-02-27 10:39:52 +0530

    Project Setup

---
+++ app/src/main/java/com/compose/android/dev/edu/iiitnr/gondi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.compose.android.dev.edu.iiitnr.gondi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
