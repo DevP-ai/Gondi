29010ac73e1e810292b31f56ac0606df6929c71f
Devajit <patar1447@gmail.com>
2024-02-27 11:12:45 +0530

    Bottom Navigation Bar implemented

---
+++ app/src/main/java/com/compose/android/dev/edu/iiitnr/gondi/ui/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.compose.android.dev.edu.iiitnr.gondi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.compose.android.dev.edu.iiitnr.gondi.R
+import com.compose.android.dev.edu.iiitnr.gondi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       val navHostFragment=supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        val navController=navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomView,navController)
+    }
+}
