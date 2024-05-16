105c4d64aa2cf58fe224fd08a1d1a1013b566879
Devajit <patar1447@gmail.com>
2024-05-17 01:08:07 +0530

    Consonant added

--- app/src/main/java/com/compose/android/dev/edu/iiitnr/gondi/ui/fragment/AlphabetFragment.kt
+++ app/src/main/java/com/compose/android/dev/edu/iiitnr/gondi/ui/fragment/AlphabetFragment.kt
@@ -42,8 +42,77 @@ class AlphabetFragment : Fragment() {
             Alphabet(R.drawable.ao, "ओ", "O", "ও"),
             Alphabet(R.drawable.um, "अं", "um", "ঔ"),
             Alphabet(R.drawable.aha, "अः", "aha", ""),
-            Alphabet(R.drawable.aha, "अः", "aha", ""),
-            Alphabet(R.drawable.aha, "अः", "aha", "")
+
+            Alphabet(R.drawable.k, "क", "K", "ক"),
+
+            Alphabet(R.drawable.kha, "ख", "Kh", "খ"),
+
+            Alphabet(R.drawable.g, "ग", "G", "গ"),
+
+            Alphabet(R.drawable.gh, "घ", "gh", "ঘ"),
+
+            Alphabet(R.drawable.ng, "ङ", "ng", "ঙ"),
+
+            Alphabet(R.drawable.ch, "च", "ch", "চ"),
+
+            Alphabet(R.drawable.chh, "छ", "chh", "ছ"),
+
+            Alphabet(R.drawable.j, "ज", "j", "জ"),
+
+            Alphabet(R.drawable.jh, "झ", "Jh", "ঝ"),
+
+            Alphabet(R.drawable.t, "ट", "T", "ট"),
+
+            Alphabet(R.drawable.th, "ठ", "Th", "ঠ"),
+
+            Alphabet(R.drawable.d, "ड", "D", "ড"),
+
+            Alphabet(R.drawable.dh, "ढ", "Dh", "ঢ"),
+
+            Alphabet(R.drawable.n, "ण", "N", "ণ"),
+
+            Alphabet(R.drawable.tt, "त", "N", "ত"),
+
+            Alphabet(R.drawable.tth, "थ", "N", "থ"),
+
+            Alphabet(R.drawable.dd, "द", "D", "দ"),
+
+            Alphabet(R.drawable.ddh, "ध", "Dh", "ধ"),
+
+            Alphabet(R.drawable.nn, "न", "N", "ন"),
+
+            Alphabet(R.drawable.p, "प", "P", "প"),
+
+            Alphabet(R.drawable.ph, "फ", "Ph", "ফ"),
+
+            Alphabet(R.drawable.b, "ब", "B", "ব"),
+
+            Alphabet(R.drawable.bh, "भ", "Bh", "ভ"),
+
+            Alphabet(R.drawable.m, "म", "M", "ম"),
+
+            Alphabet(R.drawable.y, "य", "Y", "য"),
+
+            Alphabet(R.drawable.r, "र", "R", "ৰ"),
+
+            Alphabet(R.drawable.l, "ल", "L", "ল"),
+
+            Alphabet(R.drawable.w, "व", "W", "ৱ"),
+
+            Alphabet(R.drawable.sh, "श", "Sh", "শ"),
+
+            Alphabet(R.drawable.shh, "ष", "Sh", "ষ"),
+
+            Alphabet(R.drawable.s, "स", "S", "স"),
+
+            Alphabet(R.drawable.h, "ह", "H", "হ"),
+
+            Alphabet(R.drawable.ksh, "क्ष", "Ksh", "ঞ"),
+
+            Alphabet(R.drawable.tr, "त्र", "Tr", "ড়"),
+
+            Alphabet(R.drawable.dn, "ज्ञ", "Dn", "ঢ়"),
+
         )
         adapter = AlphabetAdapter(requireContext(), dataList)
         recyclerView.adapter = adapter
